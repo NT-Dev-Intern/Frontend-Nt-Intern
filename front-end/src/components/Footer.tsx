0377e192cfd55f9386348c82662b4f0adc9fff05
PonChuenjitsiri <111419266+PonChuenjitsiri@users.noreply.github.com>
2024-06-05 14:43:59 +0700

    login-register

--- front-end/src/components/Footer.tsx
+++ front-end/src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900" aria-labelledby="footer-heading">
 
-        <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24  flex items-center">
+        <div className=" border-t border-white/10 pt-8   flex items-center justify-center">
           <p className="text-xs leading-5 text-gray-400 h-full">&copy; 2020 Your Company, Inc. All rights reserved.</p>
         </div>
       
