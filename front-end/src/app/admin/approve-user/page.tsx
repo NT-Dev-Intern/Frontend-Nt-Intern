d4cd9c126d2366f5b687345f00e5ca9dd70067be
PonChuenjitsiri <111419266+PonChuenjitsiri@users.noreply.github.com>
2024-06-25 09:05:37 +0700

    chore: Remove unused code and dependencies in approve user page

--- front-end/src/app/admin/approve-user/page.tsx
+++ front-end/src/app/admin/approve-user/page.tsx
@@ -6,23 +6,6 @@ import axios from 'axios'
 import { Label, Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 
-const people = [
-    { id: 1, name: 'Wade Cooper' },
-    { id: 2, name: 'Arlene Mccoy' },
-    { id: 3, name: 'Devon Webb' },
-    { id: 4, name: 'Tom Cook' },
-    { id: 5, name: 'Tanya Fox' },
-    { id: 6, name: 'Hellen Schmidt' },
-    { id: 7, name: 'Caroline Schultz' },
-    { id: 8, name: 'Mason Heaney' },
-    { id: 9, name: 'Claudie Smitham' },
-    { id: 10, name: 'Emil Schaefer' },
-]
-
-function classNames(...classes: any[]) {
-    return classes.filter(Boolean).join(' ')
-}
-
 const page = () => {
 
     const [pendingusers, setPendingUsers] = useState<PendingUserData[]>([])
